7325ed69d978aaf904822dda9e4d6f2e924999f9
MaximeC-L <150087360+MaximeC-L@users.noreply.github.com>
2023-11-29 20:13:02 -0500

    ajout rendre argent par piece

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -8,6 +8,8 @@ public class VendingMachine {
     private final Scanner scanner = new Scanner(System.in);
     private List<String> selectedProducts = new ArrayList<>();
     private boolean isRunning = true;
+    private final List<Integer> acceptedCoins = Arrays.asList(50, 20, 10, 5);
+
     public void start() {
         while (isRunning) {
             int choice = displayMenuActions();
@@ -16,32 +18,42 @@ public class VendingMachine {
                 case 2 -> displayPaymentOption();
                 case 3 -> deliverProduct();
                 case 4 -> cancelOrder();
-                default -> {
-                    System.out.println("L'option sélectionnée n'existe pas");
-                }
-
-            };
+                default -> System.out.println("L'option sélectionnée n'existe pas");
+            }
         }
     }
-    private void cancelOrder(){
+
+    private void giveMoneyBack() {
+        System.out.println("Rendu :");
+        for (int coin : acceptedCoins) {
+            if (balance >= coin) {
+                int nCoin = balance / coin;
+                balance -= nCoin * coin;
+                System.out.println(nCoin + " pièces de " + coin + " sous");
+            }
+
+        }
+        balance = 0;
+    }
+
+    private void cancelOrder() {
         System.out.println("Voulez-vous vraiment annuler la commande? (o/n)");
         String answer = scanner.nextLine();
-        if(answer.equals("oui") || answer.equals("o")){
-            // Todo implementer remise de l'argent
-            System.out.println("Remise de l'argent");
-            balance = 0;
+        if (answer.equals("oui") || answer.equals("o")) {
+            giveMoneyBack();
             selectedProducts.clear();
             isRunning = false;
         }
     }
-    public int displayMenuActions(){
+
+    public int displayMenuActions() {
         System.out.println("Veuillez sélectionner une option : ");
         System.out.println("1. Choisir un produit");
         System.out.println("2. Ajouter une pièce");
         System.out.println("3. Confirmer la commande");
         System.out.println("4. Annuler la commande");
 
-        return scanner.nextInt();
+        return readNumber();
 
     }
 
@@ -49,7 +61,7 @@ public class VendingMachine {
     public String selectProduct() {
         System.out.print("Veuillez sélectionner une option : ");
 
-        int choice = scanner.nextInt();
+        int choice = readNumber();
         return switch (choice) {
             case 1 -> "Coke";
             case 2 -> "Sprite";
@@ -62,6 +74,7 @@ public class VendingMachine {
     }
 
     public void displaySelectedProduct() {
+        displayMenuProducts();
         String selectedProduct = selectProduct();
         while (selectedProduct == null) {
             selectedProduct = selectProduct();
@@ -69,21 +82,23 @@ public class VendingMachine {
         selectedProducts.add(selectedProduct);
         System.out.println(selectedProduct + " a été ajouté(e) au panier");
     }
+
     public void displayPaymentOption() {
         System.out.print("Veuillez entrer les pièces");
-        int amount = scanner.nextInt();
-        List<Integer> acceptedCoins = Arrays.asList(5, 10, 20, 50);
+        int amount = readNumber();
+
         while (!acceptedCoins.contains(amount)) {
             System.out.println("Les pièces de " + amount + " ne sont pas acceptées");
             System.out.print("Veuillez entrer les pièces");
-            amount = scanner.nextInt();
+            amount = readNumber();
 
         }
         System.out.println("La pièce de " + amount + " a été reçue");
         balance += amount;
     }
 
-    public void updateBalance(String productName){
+    //Todo utiliser la fonction updateBalance
+    public void updateBalance(String productName) {
         switch (productName) {
             case "Coke" -> balance -= 25;
             case "Sprite" -> balance -= 35;
@@ -98,15 +113,25 @@ public class VendingMachine {
         System.out.println("2. Sprite - 35 cents");
         System.out.println("3. Dr.Pepper - 45 cents");
     }
-    public void deliverProduct(){
-        if(balance == 0){
+
+    public void deliverProduct() {
+        if (balance == 0) {
             // afficher liste produits dans panier
             selectedProducts.forEach(System.out::println);
+            selectedProducts.clear();
+        } else if (balance < 0) {
+            System.out.println("Veuillez insérer " + Math.abs(balance) + " sous avant de compléter la commande");
+        } else {
+            selectedProducts.forEach(System.out::println);
+            selectedProducts.clear();
+            giveMoneyBack();
+        }
+    }
 
-        }
-        else{
-            System.out.println("Veuilez insérer le bon montant d'argent avant de compléter la commande");
-        }
+    private int readNumber() {
+        int input = scanner.nextInt();
+        scanner.nextLine();
+        return input;
     }
 
     public static void main(String[] args) {
